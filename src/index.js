fed3ad4e999e81fd6cc61fa8802bbc370fc6b326
JaewookJung <stich9208@gmail.com>
2022-05-13 18:22:29 +0900

    Add: 환경세팅 완료
    Signed-off-by: JaewookJung <stich9208@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import "../styles.css";
 
-console.log("hoihoiho");
+import { App } from "./App";
+
+const main = document.getElementsByClassName("App")[0];
+
+main.innerHTML = `<h1>SPA Test</h1>${App()}`;
