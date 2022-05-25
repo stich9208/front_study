1ee7da3854f4ebc82b3f30ee809f4e1f2ac36b83
JaewookJung <stich9208@seerslab.com>
2022-05-25 23:47:04 +0900

    Add: event 작업중
    Signed-off-by: JaewookJung <stich9208@seerslab.com>

--- src/components/List.ts
+++ src/components/List.ts
@@ -2,22 +2,32 @@ import { getList, updateState, store } from "../util";
 import { productType } from "../types/data";
 
 export const List = (): string => {
+  let productsHtml = "";
+
+  //각 product Element 생성기
+  const productCreater = (elem: productType) => {
+    const liTag = document.createElement("li");
+    liTag.className = "Product";
+    liTag.innerHTML = `<img
+    src=${elem.imageUrl}
+  />
+  <div class="Product__info">
+    <div>${elem.name}</div>
+    <div>${elem.price}원~</div>
+  </div>`;
+
+    return liTag.outerHTML;
+  };
+
+  store.productList.map((product: productType) => {
+    productsHtml += productCreater(product);
+  });
+
   return store.productList.length !== 0
     ? `<div class="ProductListPage">
     <h1>상품목록</h1>
     <ul>
-    ${store.productList.map((product: productType) => {
-      return `
-      <li class="Product">
-      <img
-        src=${product.imageUrl}
-      />
-      <div class="Product__info">
-        <div>${product.name}</div>
-        <div>${product.price}원~</div>
-      </div>
-    </li>`;
-    })}
+    ${productsHtml}
     </ul>
   </div>`
     : "";
